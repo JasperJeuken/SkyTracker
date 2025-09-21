852e47a6f7fe70793caf880e6ea60acb2c9aa9cb
JasperJeuken <jeuken.jasper@gmail.com>
2025-09-21 19:26:38 +0200

    Use canvas markers for performance

--- frontend/src/components/AircraftMap.tsx
+++ frontend/src/components/AircraftMap.tsx
@@ -1,11 +1,10 @@
-// import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
-// import "leaflet/dist/leaflet.css";
-import { useEffect, useRef, useState, useCallback } from "react";
-import Map from "react-map-gl";
-import { DeckGL } from "@deck.gl/react";
-import { ScatterplotLayer } from "@deck.gl/layers";
-import 'mapbox-gl/dist/mapbox-gl.css';
+import "leaflet/dist/leaflet.css";
+import { useEffect, useRef, useState } from "react";
+import { MapContainer, TileLayer, useMapEvents } from "react-leaflet";
+import L from "leaflet";
+import MarkersCanvas from "../lib/leaflet-markers-canvas.js";
 import { getLatestBatch } from "../services/api";
+
 
 type Aircraft = {
     icao24: string;
@@ -14,91 +13,102 @@ type Aircraft = {
     heading: number;
 };
 
+function AircraftFetcher({ setAircraft }: { setAircraft: (a: Aircraft[]) => void }) {
+    const map = useMapEvents({
 
-export function AircraftMap() {
-    const [aircraft, setAircraft] = useState<Aircraft[]>([]);
-    const mapRef = useRef(null);
-    const [viewState, setViewState] = useState({
-        longitude: 4,
-        latitude: 52,
-        zoom: 6,
-        pitch: 0,
-        bearing: 0,
+        // Fetch visible states after map is moved
+        moveend: async () => {
+            const bounds = map.getBounds();
+            const ne = bounds.getNorthEast();
+            const sw = bounds.getSouthWest();
+
+            try {
+                const data = await getLatestBatch({
+                    lat_min: sw.lat,
+                    lat_max: ne.lat,
+                    lon_min: sw.lng,
+                    lon_max: ne.lng
+                });
+                setAircraft(data);
+            } catch (err) {
+                console.error(err)
+            }
+        },
     });
 
-    const fetchBatch = useCallback(async () => {
-        if (!mapRef.current) {
-            return null;
-        }
-        const mapInstance = mapRef.current.getMap();
-        const bounds = mapInstance.getBounds();
-        const ne = bounds.getNorthEast();
-        const sw = bounds.getSouthWest();
-
-        try {
-            const data = await getLatestBatch({
-                lat_min: sw.lat,
-                lat_max: ne.lat,
-                lon_min: sw.lng,
-                lon_max: ne.lng,
-            });
-            setAircraft(data);
-        } catch (err) {
-            console.error(err);
-        }
-    }, []);
-
+    // Fetch initial data on component mount
     useEffect(() => {
-        fetchBatch();
-    }, [fetchBatch]);
+        map.fire('moveend');
+    }, [map]);
 
-    const layers = [
-        new ScatterplotLayer({
-            id: "aircraft-layer",
-            data: aircraft,
-            pickable: true,
-            getPosition: (d: Aircraft) => [d.longitude, d.latitude],
-            getFillColor: [255, 0, 0],
-            getRadius: 20000,
-            radiusMinPixels: 2,
-            radiusMaxPixels: 10,
-        }),
-    ];
-
-    return (
-        <DeckGL
-            viewState={viewState}
-            controller={true}
-            layers={layers}
-            onViewStateChange={({ viewState }) => {setViewState(viewState)}}
-            style={{ width: "100vw", height: "100vh" }}
-        >
-            {/* TODO: fix Map */}
-            <Map
-                ref={mapRef}
-                reuseMaps
-                mapLib={import("maplibre-gl")}
-                mapStyle="https://demotiles.maplibre.org/style.json"
-                onMoveEnd={fetchBatch}
-            />
-        </DeckGL>
-    );
+    return null;
 }
 
 
-// export function AircraftMap() {
-//     const [aircraft, setAircraft] = useState<Aircraft[]>([]);
+function CanvasMarkersLayer({ aircraft }: { aircraft: Aircraft[] }) {
+    // Get map instance and create reference for marker canvas
+    const map = useMapEvents({});
+    const markersCanvasRef = useRef<any>(null);
 
-//     return (
-//         <MapContainer style={{ height: "100vh", width: "100%"}} center={[52, 3]} zoom={10}>
-//             <TileLayer
-//                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-//             />
-//             <AircraftFetcher setAircraft={setAircraft} />
-//             {aircraft.map((a) => (
-//                 <Marker key={a.icao24} position={[a.latitude, a.longitude]}></Marker>
-//             ))}
-//         </MapContainer>
-//     );
-// }
+    // Initialize and cleanup of canvas layer
+    useEffect(() => {
+
+        // Initialize canvas marker layer
+        if (!map) return;
+        const canvasLayer = new MarkersCanvas();
+        canvasLayer.addTo(map);
+        markersCanvasRef.current = canvasLayer;
+
+        // Cleanup function
+        return () => {
+            if (map && canvasLayer) {
+                map.removeLayer(canvasLayer);
+            }
+        };
+    }, [map]);
+
+    // Update markers when aircraft data changes
+    useEffect(() => {
+        // Clear existing markers
+        const layer = markersCanvasRef.current;
+        if (!layer) return;
+        layer.clear();
+        if (!aircraft || aircraft.length === 0) return;
+
+        // Create marker for each aircraft (rotated with heading)
+        const markers = aircraft.map(a => {
+            const svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="#1E90FF" d="M21 15.984l-8.016-4.5V3.516a1.5 1.5 0 0 0-3 0v7.969L2.016 15.984V18l8.016-2.484V21l-2.016 1.5V24l3.516-0.984L15 22.5v-1.5L13.031 21v-5.484L21 18v-2.016z"/></svg>`;
+            const iconUrl = `data:image/svg+xml;charset=utf-8,${encodeURIComponent(svg)}`;
+            const icon = L.icon({
+                iconUrl,
+                iconSize: [24, 24],
+                iconAnchor: [12, 12],
+                rotationAngle: a.heading,
+            });
+            return L.marker(
+                [a.latitude, a.longitude],
+                { icon }
+            ).bindPopup(`ICAO24: ${a.icao24}<br>Heading: ${a.heading.toFixed(0)}°`);
+        });
+
+        // Add markers to canvas layer
+        layer.addMarkers(markers);
+    }, [aircraft]);
+
+    return null;
+}
+
+export function AircraftMap() {
+    const [aircraft, setAircraft] = useState<Aircraft[]>([]);
+
+    return (
+        <MapContainer style={{ height: "100vh", width: "100%"}} center={[52, 4]} zoom={6}>
+            <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            />
+            <AircraftFetcher setAircraft={setAircraft} />
+            <CanvasMarkersLayer aircraft={aircraft} />
+        </MapContainer>
+    );
+}
---
+++ frontend/src/lib/leaflet-markers-canvas.js
@@ -0,0 +1,361 @@
+/**
+ * leaflet-markers-canvas (copied/converted for ESM)
+ * Original: https://github.com/francoisromain/leaflet-markers-canvas
+ * Author: francoisromain (give attribution per original license)
+ *
+ * Converted to ESM so it can be imported directly by Vite/ESM apps.
+ */
+
+import L from "leaflet";
+import RBush from "rbush";
+
+const markersCanvas = {
+  _map: null,
+  _canvas: null,
+  _context: null,
+  _markers: [],
+  _markersTree: new RBush(),
+  _positionsTree: new RBush(),
+  _icons: {},
+
+  addTo(map) {
+    map.addLayer(this);
+    return this;
+  },
+
+  getBounds() {
+    const bounds = new L.LatLngBounds();
+    this._markers.forEach((marker) => bounds.extend(marker.getLatLng()));
+    return bounds;
+  },
+
+  redraw() {
+    this._redraw(true);
+  },
+
+  clear() {
+    this._positionsTree = new RBush();
+    this._markersTree = new RBush();
+    this._markers = [];
+    this._redraw(true);
+  },
+
+  addMarker(marker) {
+    const { markerBox, positionBox, isVisible } = this._addMarker(marker);
+    if (markerBox && isVisible) this._markersTree.insert(markerBox);
+    if (positionBox) this._positionsTree.insert(positionBox);
+  },
+
+  addMarkers(markers) {
+    const markerBoxes = [];
+    const positionBoxes = [];
+    markers.forEach((marker) => {
+      const { markerBox, positionBox, isVisible } = this._addMarker(marker);
+      if (markerBox && isVisible) markerBoxes.push(markerBox);
+      if (positionBox) positionBoxes.push(positionBox);
+    });
+    this._markersTree.load(markerBoxes);
+    this._positionsTree.load(positionBoxes);
+  },
+
+  removeMarker(marker) {
+    const latLng = marker.getLatLng();
+    const isVisible = this._map.getBounds().contains(latLng);
+
+    const positionBox = {
+      minX: latLng.lng,
+      minY: latLng.lat,
+      maxX: latLng.lng,
+      maxY: latLng.lat,
+      marker,
+    };
+
+    this._positionsTree.remove(positionBox, (a, b) => {
+      return a.marker._leaflet_id === b.marker._leaflet_id;
+    });
+
+    if (isVisible) this._redraw(true);
+  },
+
+  removeMarkers(markers) {
+    let hasChanged = false;
+    markers.forEach((marker) => {
+      const latLng = marker.getLatLng();
+      const isVisible = this._map.getBounds().contains(latLng);
+      const positionBox = {
+        minX: latLng.lng,
+        minY: latLng.lat,
+        maxX: latLng.lng,
+        maxY: latLng.lat,
+        marker,
+      };
+      this._positionsTree.remove(positionBox, (a, b) => {
+        return a.marker._leaflet_id === b.marker._leaflet_id;
+      });
+      if (isVisible) hasChanged = true;
+    });
+    if (hasChanged) this._redraw(true);
+  },
+
+  initialize(options) {
+    L.Util.setOptions(this, options);
+  },
+
+  onAdd(map) {
+    this._map = map;
+    this._initCanvas();
+    this.getPane().appendChild(this._canvas);
+
+    map.on("moveend", this._reset, this);
+    map.on("resize", this._reset, this);
+
+    map.on("click", this._fire, this);
+    map.on("mousemove", this._fire, this);
+
+    if (map._zoomAnimated) {
+      map.on("zoomanim", this._animateZoom, this);
+    }
+  },
+
+  onRemove(map) {
+    this.getPane().removeChild(this._canvas);
+
+    map.off("click", this._fire, this);
+    map.off("mousemove", this._fire, this);
+    map.off("moveend", this._reset, this);
+    map.off("resize", this._reset, this);
+
+    if (map._zoomAnimated) {
+      map.off("zoomanim", this._animateZoom, this);
+    }
+  },
+
+  setOptions(options) {
+    L.Util.setOptions(this, options);
+    return this.redraw();
+  },
+
+  _initCanvas() {
+    const { x, y } = this._map.getSize();
+    const isAnimated = this._map.options.zoomAnimation && L.Browser.any3d;
+
+    this._canvas = L.DomUtil.create(
+      "canvas",
+      "leaflet-markers-canvas-layer leaflet-layer"
+    );
+    this._canvas.width = x;
+    this._canvas.height = y;
+    this._canvas.style.width = `${x}px`;
+    this._canvas.style.height = `${y}px`;
+    this._context = this._canvas.getContext("2d");
+
+    L.DomUtil.addClass(
+      this._canvas,
+      `leaflet-zoom-${isAnimated ? "animated" : "hide"}`
+    );
+  },
+
+  _addMarker(marker) {
+    if (marker.options.pane !== "markerPane" || !marker.options.icon) {
+      console.error("This is not a marker", marker);
+      return { markerBox: null, positionBox: null, isVisible: null };
+    }
+
+    marker._map = this._map;
+    L.Util.stamp(marker);
+
+    const latLng = marker.getLatLng();
+    const isVisible = this._map.getBounds().contains(latLng);
+    const { x, y } = this._map.latLngToContainerPoint(latLng);
+    const { iconSize, iconAnchor } = marker.options.icon.options;
+
+    const markerBox = {
+      minX: x - iconAnchor[0],
+      minY: y - iconAnchor[1],
+      maxX: x + iconSize[0] - iconAnchor[0],
+      maxY: y + iconSize[1] - iconAnchor[1],
+      marker,
+    };
+
+    const positionBox = {
+      minX: latLng.lng,
+      minY: latLng.lat,
+      maxX: latLng.lng,
+      maxY: latLng.lat,
+      marker,
+    };
+
+    if (isVisible) this._drawMarker(marker, { x, y });
+
+    this._markers.push(marker);
+
+    return { markerBox, positionBox, isVisible };
+  },
+
+  _drawMarker(marker, { x, y }) {
+    const { iconUrl } = marker.options.icon.options;
+
+    if (marker.image) {
+      this._drawImage(marker, { x, y });
+    } else if (this._icons[iconUrl]) {
+      marker.image = this._icons[iconUrl].image;
+
+      if (this._icons[iconUrl].isLoaded) {
+        this._drawImage(marker, { x, y });
+      } else {
+        this._icons[iconUrl].elements.push({ marker, x, y });
+      }
+    } else {
+      const image = new Image();
+      image.src = iconUrl;
+      marker.image = image;
+
+      this._icons[iconUrl] = {
+        image,
+        isLoaded: false,
+        elements: [{ marker, x, y }],
+      };
+
+      image.onload = () => {
+        this._icons[iconUrl].isLoaded = true;
+        this._icons[iconUrl].elements.forEach(({ marker, x, y }) => {
+          this._drawImage(marker, { x, y });
+        });
+      };
+    }
+  },
+
+  _drawImage(marker, { x, y }) {
+    const { rotationAngle, iconAnchor, iconSize } = marker.options.icon.options;
+    const angle = rotationAngle || 0;
+
+    this._context.save();
+    this._context.translate(x, y);
+    this._context.rotate((angle * Math.PI) / 180);
+    this._context.drawImage(
+      marker.image,
+      -iconAnchor[0],
+      -iconAnchor[1],
+      iconSize[0],
+      iconSize[1]
+    );
+    this._context.restore();
+  },
+
+  _redraw(clear) {
+    if (clear) {
+      this._context.clearRect(0, 0, this._canvas.width, this._canvas.height);
+    }
+
+    if (!this._map || !this._positionsTree) return;
+
+    const mapBounds = this._map.getBounds();
+    const mapBoundsBox = {
+      minX: mapBounds.getWest(),
+      minY: mapBounds.getSouth(),
+      maxX: mapBounds.getEast(),
+      maxY: mapBounds.getNorth(),
+    };
+
+    const markers = [];
+    this._positionsTree.search(mapBoundsBox).forEach(({ marker }) => {
+      const latLng = marker.getLatLng();
+      const { x, y } = this._map.latLngToContainerPoint(latLng);
+      const { iconSize, iconAnchor } = marker.options.icon.options;
+
+      const markerBox = {
+        minX: x - iconAnchor[0],
+        minY: y - iconAnchor[1],
+        maxX: x + iconSize[0] - iconAnchor[0],
+        maxY: y + iconSize[1] - iconAnchor[1],
+        marker,
+      };
+
+      markers.push(markerBox);
+      this._drawMarker(marker, { x, y });
+    });
+
+    this._markersTree.clear();
+    this._markersTree.load(markers);
+  },
+
+  _reset() {
+    const topLeft = this._map.containerPointToLayerPoint([0, 0]);
+    L.DomUtil.setPosition(this._canvas, topLeft);
+
+    const { x, y } = this._map.getSize();
+    this._canvas.width = x;
+    this._canvas.height = y;
+
+    this._redraw();
+  },
+
+  _fire(event) {
+    if (!this._markersTree) return;
+
+    const { x, y } = event.containerPoint;
+    const markers = this._markersTree.search({
+      minX: x,
+      minY: y,
+      maxX: x,
+      maxY: y,
+    });
+
+    if (markers && markers.length) {
+      this._map._container.style.cursor = "pointer";
+      const marker = markers[0].marker;
+
+      if (event.type === "click") {
+        if (marker.listens("click")) {
+          marker.fire("click");
+        }
+      }
+
+      if (event.type === "mousemove") {
+        if (this._mouseOverMarker && this._mouseOverMarker !== marker) {
+          if (this._mouseOverMarker.listens("mouseout")) {
+            this._mouseOverMarker.fire("mouseout");
+          }
+        }
+
+        if (!this._mouseOverMarker || this._mouseOverMarker !== marker) {
+          this._mouseOverMarker = marker;
+          if (marker.listens("mouseover")) {
+            marker.fire("mouseover");
+          }
+        }
+      }
+    } else {
+      this._map._container.style.cursor = "";
+      if (event.type === "mousemove" && this._mouseOverMarker) {
+        if (this._mouseOverMarker.listens("mouseout")) {
+          this._mouseOverMarker.fire("mouseout");
+        }
+        delete this._mouseOverMarker;
+      }
+    }
+  },
+
+  _animateZoom(event) {
+    const scale = this._map.getZoomScale(event.zoom);
+    const offset = this._map._latLngBoundsToNewLayerBounds(
+      this._map.getBounds(),
+      event.zoom,
+      event.center
+    ).min;
+
+    L.DomUtil.setTransform(this._canvas, offset, scale);
+  },
+};
+
+const MarkersCanvas = L.Layer.extend(markersCanvas);
+
+// attach to L and window for backward compatibility
+L.MarkersCanvas = MarkersCanvas;
+if (typeof window !== "undefined") {
+  window.L = window.L || L;
+  window.L.MarkersCanvas = MarkersCanvas;
+}
+
+export default MarkersCanvas;
+export { MarkersCanvas };
